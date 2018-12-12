f7a45911f9e910f5136ca4dc86c6bb3a2ae34019
Zexrica <krij0046@hz.nl>
2018-12-12 14:44:04 +0100

    mapEurope toegevoegd en EuropeScreen update

--- src/view/EuropeScreen.ts
+++ src/view/EuropeScreen.ts
@@ -1,11 +1,17 @@
-class EuropeScreen extends Mapview{
+class EuropeScreen extends Mapview {
 
-    constructor(canvas:HTMLCanvasElement){
+    private europeMap: HTMLImageElement
+
+    constructor(canvas: HTMLCanvasElement) {
         super(canvas);
+        this.canvas.colorClick()
+    //     this.europeMap = new Image()
+    //     this.europeMap.src = './assets/images/mapEurope.png'
     }
 
-    public draw(){
-        
+    public draw() {
+        this.canvas.writeImageToCanvas('./assets/images/mapEurope.png', 48, 73, this.canvas.getHeight() - 73, this.canvas.getHeight() - 73)
     }
+    
     
 }
