f0b596a28e90cf87db343890af7a3e31a9cfe95b
xAmberWinchester <42797857+xAmberWinchester@users.noreply.github.com>
2018-12-13 11:24:45 +0100

    pictures added to start screen and put player name in player

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -17,4 +17,8 @@ class Player {
     public scorePlus(plusAmount: number) {
         this.score += plusAmount
     }
+
+    public setName(name: string) {
+        this.name = name
+    }
 }
--- src/view/StartScreen.ts
+++ src/view/StartScreen.ts
@@ -1,29 +1,31 @@
 class StartScreen {
     private canvas: Canvas;
     public buttonPressed: boolean;
-    private game: Game;
     private europeScreen: EuropeScreen;
+    private player: Player;
 
     constructor() {
         const canvasElement: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canvas');
         this.canvas = new Canvas(canvasElement);
         this.europeScreen = new EuropeScreen(canvasElement);
+        this.player = new Player();
     }
 
     public draw() {
-        this.canvas.writeTextToCanvas('Waar is deze geschiedenis?', 125, this.canvas.getWidth() / 2, this.canvas.getHeight() / 4.3, 'black', 'center', "Old English Text MT");
-        this.canvas.writeButtonToCanvas("../assets/images/buttonYellow.png", this.canvas.getCenter().X - 111, this.canvas.getCenter().Y + 100, "Start", 35, this.canvas.getCenter().X, this.canvas.getCenter().Y + 130, "black", "center", "Old English Text MT");
+        this.canvas.writeTextToCanvas('Waar is deze geschiedenis?', 125, this.canvas.getWidth() / 2, this.canvas.getHeight() / 4.2, 'black', 'center', "Old English Text MT");
+        this.canvas.writeButtonToCanvas("../assets/images/buttonYellow.png", this.canvas.getCenter().X - 111, this.canvas.getCenter().Y + 250, "Start", 35, this.canvas.getCenter().X, this.canvas.getCenter().Y + 280, "black", "center", "Old English Text MT");
         this.buttonPressed = false;
         window.addEventListener("click", (event: MouseEvent) => {
             if (this.buttonPressed == false) {
                 if (event.x > this.canvas.getCenter().X - 111 && event.x < this.canvas.getCenter().X + 111) {
-                    if (event.y > this.canvas.getCenter().Y + 100 && event.y < this.canvas.getCenter().Y + 139) {
-                        let names = (<HTMLInputElement>document.getElementById('name'));
+                    if (event.y > this.canvas.getCenter().Y + 250 && event.y < this.canvas.getCenter().Y + 289) {
+                        let names = (<HTMLInputElement>document.getElementById('name')); 
                         console.log(names.value)
-                        names.type = "hidden"
+                        names.type = "hidden";  
+                        document.getElementById("wereld").setAttribute('style', 'hidden')
                         this.canvas.clearScreen();
                         this.europeScreen.draw();
-                        //hier functie voor de pagina.
+                        this.player.setName(names.value);
                         this.buttonPressed = true;
 
                     }
