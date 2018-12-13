02d40c62c344b26f6f76be4e82e03ad3ed999563
xAmberWinchester <42797857+xAmberWinchester@users.noreply.github.com>
2018-12-13 09:16:03 +0100

    textblok to type name

--- src/view/StartScreen.ts
+++ src/view/StartScreen.ts
@@ -13,9 +13,14 @@ class StartScreen {
     public draw(){
         this.canvas.writeTextToCanvas('Waar is deze geschiedenis?', 125, this.canvas.getWidth()/ 2, this.canvas.getHeight()/ 4.3, 'white', 'center', "Old English Text MT");
         this.canvas.writeButtonToCanvas("../assets/images/buttonYellow.png",this.canvas.getCenter().X -111, this.canvas.getCenter().Y + 100,"Start",35,this.canvas.getCenter().X,this.canvas.getCenter().Y + 130,"black","center", "Old English Text MT");
+        
         window.addEventListener("click", (event: MouseEvent) => {
+   
             if (event.x > this.canvas.getCenter().X -111  && event.x < this.canvas.getCenter().X + 111) {
                 if (event.y > this.canvas.getCenter().Y +100 && event.y < this.canvas.getCenter().Y +139) {
+                    let names = (<HTMLInputElement>document.getElementById('name'));      
+                    console.log(names.value)
+                    names.type = "hidden"
                     this.canvas.clearScreen();
                     this.europeScreen.draw();
                     //hier functie voor de pagina.
