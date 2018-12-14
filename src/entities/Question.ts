f908a1de3a5529f35a3151aff166b9216c897c7b
Tim Klomp <klom0006@hz.nl>
2018-12-14 09:35:36 +0100

    added the possibility to answer the question in CountryScreen

--- src/entities/Question.ts
+++ src/entities/Question.ts
@@ -1,3 +1,35 @@
 class Question {
-    
+
+    constructor() {
+
+    }
+    public getQuestion(index: number): { question: string, answer: string } {
+        let quiz = [
+            {
+                question: "klik de provincie aan waar Zwolle ligt",
+                answer: "Overijsel"
+            },
+            {
+                question: "klik de provincie aan waar Den Bosch ligt",
+                answer: "Noord-Brabant"
+            },
+            {
+                question: "klik de provincie aan waar Amsterdam ligt",
+                answer: "Noord-Holland"
+            },
+            {
+                question: "klik de provincie aan waar Groningen ligt",
+                answer: "Groningen"
+            },
+        ]
+        return { question: quiz[index].question, answer: quiz[index].answer }
+
+
+
+
+
+
+
+
+    }
 }
