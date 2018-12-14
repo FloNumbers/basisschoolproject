ef76ae1504d12900dd7b184bcad5ae82ae3e80f1
xAmberWinchester <42797857+xAmberWinchester@users.noreply.github.com>
2018-12-14 09:07:35 +0100

    position fixed html elements

--- src/view/StartScreen.ts
+++ src/view/StartScreen.ts
@@ -15,15 +15,26 @@ class StartScreen {
         this.canvas.writeTextToCanvas('Waar is deze geschiedenis?', 125, this.canvas.getWidth() / 2, this.canvas.getHeight() / 4.2, 'black', 'center', "Old English Text MT");
         this.canvas.writeButtonToCanvas("../assets/images/buttonYellow.png", this.canvas.getCenter().X - 111, this.canvas.getCenter().Y + 250, "Start", 35, this.canvas.getCenter().X, this.canvas.getCenter().Y + 280, "black", "center", "Old English Text MT");
         this.buttonPressed = false;
+
+        document.getElementById("wereld").style.top = '25%'; /* DIT DOEN VOOR PLAATSING */
+        document.getElementById('wereld').style.left = '40.6%';
+
+        document.getElementById('name').style.top = '65%';
+        document.getElementById('name').style.left = '40.2%';
+
+        document.getElementById('compass').style.top = '60%';
+        document.getElementById('compass').style.left = '6.5%';
+
         window.addEventListener("click", (event: MouseEvent) => {
             if (this.buttonPressed == false) {
                 if (event.x > this.canvas.getCenter().X - 111 && event.x < this.canvas.getCenter().X + 111) {
                     if (event.y > this.canvas.getCenter().Y + 250 && event.y < this.canvas.getCenter().Y + 289) {
                         let names = (<HTMLInputElement>document.getElementById('name')); 
+
                         console.log(names.value);
                         names.type = "hidden";  
                         document.getElementById("wereld").setAttribute('style', 'hidden');
-                        document.getElementById("compass").setAttribute('style', 'hidden')
+                        document.getElementById("compass").setAttribute('style', 'hidden');
                         this.canvas.clearScreen();
                         this.europeScreen.draw();
                         this.player.setName(names.value);
