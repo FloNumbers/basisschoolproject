684171f66801692373eea5900b62c0912165d6ac
FloNumbers <floriansole@gmail.com>
2018-12-20 09:52:30 +0100

    shopscreen fix and countryscreen back to europe button added

--- src/view/CountryScreen.ts
+++ src/view/CountryScreen.ts
@@ -27,13 +27,31 @@ class CountryScreen extends Mapview {
         // this.canvas.writeImageToCanvas(this.questionImage.src, this.canvas.getWidth() / 1.43, 90, this.canvas.getWidth() / 4, this.canvas.getHeight() / 1.1)
         // this.canvas.writeTextToCanvas(`${this.player.getName()}`, 30, this.canvas.getWidth() / 6, this.canvas.getHeight() / 4, 'black', 'center', 'Pristina')
         this.canvas.writeButtonToCanvas(this.shopButton.src, this.canvas.getWidth() / 1.4, this.canvas.getHeight() / 1.35, "Winkel", 35, this.canvas.getWidth() / 1.28, this.canvas.getHeight() / 1.18, "black", "center", "Old English Text MT");
+        this.toShopHandler()
         this.canvas.writeTextToCanvas(`${this.question.getQuestion(this.canvas.getquestionNumber()).introToQuestion}`, 30, this.canvas.getWidth() / 1.35, this.canvas.getHeight() / 3.7, 'black', 'center', 'Pristina')
         this.canvas.writeTextToCanvas(`${this.question.getQuestion(this.canvas.getquestionNumber()).introToQuestion2}`, 30, this.canvas.getWidth() / 1.35, this.canvas.getHeight() / 3.2, 'black', 'center', 'Pristina')
         this.canvas.writeTextToCanvas(`${this.question.getQuestion(this.canvas.getquestionNumber()).introToQuestion3}`, 30, this.canvas.getWidth() / 1.35, this.canvas.getHeight() / 2.8, 'black', 'center', 'Pristina')
         this.canvas.writeTextToCanvas(`${this.question.getQuestion(this.canvas.getquestionNumber()).introToQuestion4}`, 30, this.canvas.getWidth() / 1.35, this.canvas.getHeight() / 2.5, 'black', 'center', 'Pristina')
         this.canvas.writeTextToCanvas(`${this.question.getQuestion(this.canvas.getquestionNumber()).introToQuestion5}`, 30, this.canvas.getWidth() / 1.35, this.canvas.getHeight() / 2.2, 'black', 'center', 'Pristina')
         this.canvas.writeTextToCanvas(`${this.question.getQuestion(this.canvas.getquestionNumber()).question}`, 30, this.canvas.getWidth() / 1.35, this.canvas.getHeight() / 1.9, 'black', 'center', 'Pristina')
+        this.canvas.writeButtonToCanvas(this.shopButton.src, this.canvas.getWidth() / 1.8, this.canvas.getHeight() / 1.35, "Terug", 35, this.canvas.getWidth() / 1.62, this.canvas.getHeight() / 1.18, "black", "center", "Old English Text MT");
+        this.backToEuropeHandler()
 
+    }
+
+    public backToEuropeHandler() {
+        let listenerBackToEurope = (event: MouseEvent) => {
+            if (event.x > this.canvas.getWidth() / 1.8 && event.x < this.canvas.getWidth() / 1.8 + this.shopButton.width) {
+                if (event.y > this.canvas.getHeight() / 1.3 && event.y < this.canvas.getHeight() / 1.4 + this.shopButton.height) {
+                    ScreenSwitch.draw('europeScreen')
+                }
+            }
+        }
+        this.listeners.push(listenerBackToEurope)
+        window.addEventListener('click', listenerBackToEurope)
+    }
+
+    public toShopHandler() {
         let listenerToShop = (event: MouseEvent) => {
             if (event.x > this.canvas.getWidth() / 1.4 && event.x < this.canvas.getWidth() / 1.4 + this.shopButton.width) {
                 if (event.y > this.canvas.getHeight() / 1.3 && event.y < this.canvas.getHeight() / 1.4 + this.shopButton.height) {
@@ -45,11 +63,10 @@ class CountryScreen extends Mapview {
         }
         this.listeners.push(listenerToShop)
         window.addEventListener('click', listenerToShop)
-
     }
 
     private removeButtons() {
-        this.listeners.forEach(e =>{
+        this.listeners.forEach(e => {
             window.removeEventListener('click', e);
         });
     }
--- src/view/ShopScreen.ts
+++ src/view/ShopScreen.ts
@@ -7,18 +7,16 @@ class ShopScreen {
     constructor() {
         const canvasElement: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canvas');
         this.canvas = new Canvas(canvasElement)
-        this.player = new Player()
         this.listeners = [];
     }
     public draw() {
-        console.log('test')
         this.canvas.writeTextToCanvas('Winkel', 150, this.canvas.getCenter().X, this.canvas.getCenter().Y - 260, "black", "center", "Old English Text MT")
         this.canvas.writeTextToCanvas('Hints :', 100, this.canvas.getCenter().X - 399, this.canvas.getCenter().Y - 100, "black", 'center', "Old English Text MT")
         this.drawShopBoxes(200, 175)
         this.hintShopHandler(200, 175)
-        this.canvas.writeTextToCanvas('Background :', 100, this.canvas.getCenter().X - 527, this.canvas.getCenter().Y + 100, "black", 'center', "Old English Text MT")
+        this.canvas.writeTextToCanvas('Achtergrond :', 100, this.canvas.getCenter().X - 525, this.canvas.getCenter().Y + 100, "black", 'center', "Old English Text MT")
         this.drawShopBoxes(200, -25)
-        this.canvas.writeTextToCanvas('Music :', 100, this.canvas.getCenter().X - 410, this.canvas.getCenter().Y + 300, "black", "center", "Old English Text MT")
+        this.canvas.writeTextToCanvas('Muziek :', 100, this.canvas.getCenter().X - 430, this.canvas.getCenter().Y + 300, "black", "center", "Old English Text MT")
         this.drawShopBoxes(200, -225)
         this.canvas.writeButtonToCanvas("./assets/images/oldButton.png", this.canvas.getCenter().X + 325, this.canvas.getCenter().Y + 175, "terug", 35, this.canvas.getCenter().X + 450, this.canvas.getCenter().Y + 260, "black", "center", "Old English Text MT");
         this.shopOutHandler()
@@ -42,22 +40,23 @@ class ShopScreen {
     }
 
     public hintShopHandler(xBox: number, yBox: number) {
-        let points = this.player.getScore()
         for (let index = 0; index < 3; index++) {
-            this.canvas.writeTextToCanvas(`You have ${this.hints} hints`, 50, this.canvas.getCenter().X + 450, this.canvas.getCenter().Y - 100, "black", "center", "Old English Text MT")
-            this.canvas.writeTextToCanvas(`${this.player.getScore()} points`, 50, this.canvas.getCenter().X - 410, this.canvas.getCenter().Y - 250, "black", "center", "Old English Text MT")
+            let isClicked = 0;
+            this.canvas.writeTextToCanvas(`Je hebt ${this.hints} hints`, 50, this.canvas.getCenter().X + 450, this.canvas.getCenter().Y - 100, "black", "center", "Old English Text MT")
+            this.canvas.writeTextToCanvas(`${PlayerHandler.getScore()} punten`, 50, this.canvas.getCenter().X - 410, this.canvas.getCenter().Y - 250, "black", "center", "Old English Text MT")
             let listenerHintShop = (event: MouseEvent) => {
                 if (event.x > this.canvas.getCenter().X - xBox + (index * 130) && event.x < this.canvas.getCenter().X - xBox + 100 + (index * 130)) {
                     if (event.y > this.canvas.getCenter().Y - yBox && event.y < this.canvas.getCenter().Y - yBox + 100) {
-                        if (this.player.getScore() >= 10) {
+                        if (PlayerHandler.getScore() >= 10 && isClicked == 0) {
                             this.canvas.clearArea(this.canvas.getCenter().X + 250, this.canvas.getCenter().Y - 140, 400, 100)
                             this.canvas.clearArea(this.canvas.getCenter().X - 550, this.canvas.getCenter().Y - 300, 300, 100)
+                            isClicked = 1
                             if (this.hints < 3) {
-                                this.player.scoreMinus(10)
+                                PlayerHandler.scoreMinus(10)
                                 this.hints++
                             }
-                            this.canvas.writeTextToCanvas(`You have ${this.hints} hints`, 50, this.canvas.getCenter().X + 450, this.canvas.getCenter().Y - 100, "black", "center", "Old English Text MT")
-                            this.canvas.writeTextToCanvas(`${this.player.getScore()} points`, 50, this.canvas.getCenter().X - 410, this.canvas.getCenter().Y - 250, "black", "center", "Old English Text MT")
+                            this.canvas.writeTextToCanvas(`Je hebt ${this.hints} hints`, 50, this.canvas.getCenter().X + 450, this.canvas.getCenter().Y - 100, "black", "center", "Old English Text MT")
+                            this.canvas.writeTextToCanvas(`${PlayerHandler.getScore()} punten`, 50, this.canvas.getCenter().X - 410, this.canvas.getCenter().Y - 250, "black", "center", "Old English Text MT")
                             console.log(this.hints)
                         }
                     }
