b48a105027e343a5f55fe29694a0c5a00a164b89
Tim Klomp <klom0006@hz.nl>
2019-01-08 13:43:44 +0100

    Revert "Updated EuropeScreen"
    This reverts commit a7741cf11247c1c9ba6606e79d66d0abd4832329.

--- src/view/EuropeScreen.ts
+++ src/view/EuropeScreen.ts
@@ -14,14 +14,12 @@ class EuropeScreen extends Mapview {
     public draw() {
         this.canvas.writeImageToCanvasPreload(this.europeMap, this.canvas.getWidth() / 28, this.canvas.getWidth() / 19, this.canvas.getHeight() - this.canvas.getHeight() / 9.3, this.canvas.getHeight() - this.canvas.getHeight() / 9.3)
         this.canvas.getSelectedCountry()
+        console.log(this.canvas.getSelectedCountry())
         this.canvas.writeTextToCanvas('Kies een land om te beginnen!', 35, this.canvas.getWidth() / 1.35, this.canvas.getHeight() / 4, "black", "center", "Old English Text MT")
         this.clickCountry()
         this.canvas.writeButtonToCanvas('./assets/images/settings-button.png', this.canvas.getCenter().X + 610, this.canvas.getCenter().Y + 300, " ", 35, this.canvas.getCenter().X, this.canvas.getCenter().Y, 'black', 'center', 'ariel');
         this.optionButton();
         ScreenSwitch.screenMemory('europeScreen')
-
-        this.canvas.writeButtonToCanvas("./assets/images/oldButton.png", this.canvas.getCenter().X + 350, this.canvas.getCenter().Y + 200, "Save", 35, this.canvas.getCenter().X + 475, this.canvas.getCenter().Y + 290, 'black', "center", "Old English Text MT");
-        this.saveButton()
     }
 
     private removeButtons() {
@@ -46,26 +44,12 @@ class EuropeScreen extends Mapview {
         window.addEventListener('click', listenerToCountry)
     }
 
-    private optionButton() {
+    public optionButton() {
         let startListener = (event: MouseEvent) => {
             if (event.x > this.canvas.getCenter().X + 610 && event.x < this.canvas.getCenter().X + 685) {
                 if (event.y > this.canvas.getCenter().Y + 300 && event.y < this.canvas.getCenter().Y + 375) {
                     ScreenSwitch.draw('optionsScreen');
                     this.removeButtons();
-                }
-            }
-        }
-        this.listeners.push(startListener)
-        window.addEventListener('click', startListener)
-    }
-
-    private saveButton() {
-        let startListener = (event: MouseEvent) => {
-            if (event.x > this.canvas.getCenter().X + 350 && event.x < this.canvas.getCenter().X + 600) {
-                if (event.y > this.canvas.getCenter().Y + 200 && event.y < this.canvas.getCenter().Y + 370) {
-                    PlayerHandler.setSaveStatus('overwrite')
-                    ScreenSwitch.draw('saveScreen')
-                    this.removeButtons()
                 }
             }
         }
